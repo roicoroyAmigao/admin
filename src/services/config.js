2ca0dd77652c8414fe783141042193b9175a4bbb
Ricardo <106403365+roicoroyAmigao@users.noreply.github.com>
2023-01-11 17:56:56 +0000

    z

--- src/services/config.js
+++ src/services/config.js
@@ -1,3 +1,5 @@
-const medusaUrl = __MEDUSA_BACKEND_URL__ || "http://localhost:9000"
+// eslint-disable-next-line prettier/prettier
+// const medusaUrl = __MEDUSA_BACKEND_URL__ || "https://amigao-medusa-api.onrender.com"
+const medusaUrl = "https://amigao-medusa-api.onrender.com"
 
 export { medusaUrl }
